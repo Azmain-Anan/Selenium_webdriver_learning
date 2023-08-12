d6fae5ac1cb426b21aceda0d26c54ba0baac137b
Azmain Anan <azmainanan1998@gmail.com>
2023-08-12 21:14:05 +0600

    Nineth commit

---
+++ Auto2/src/test/java/ui/dragDrop.java
@@ -0,0 +1,43 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class dragDrop {
+
+	public static String browser = "Chrome";
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		if (browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equals("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equals("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
+		driver.get("https://jqueryui.com/droppable/");
+		driver.manage().window().maximize();
+		WebElement frame = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[1]/iframe"));
+		driver.switchTo().frame(frame);
+		
+		WebElement drag = driver.findElement(By.id("draggable"));
+		WebElement drop = driver.findElement(By.id("droppable"));
+		
+		Actions action = new Actions(driver);
+		action.dragAndDrop(drag, drop).perform();
+
+	}
+
+}
