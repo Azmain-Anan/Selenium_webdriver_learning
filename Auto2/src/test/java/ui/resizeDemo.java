ba480aa022df6dcb74c7d3d16dd77bfdc7d42320
Azmain Anan <azmainanan1998@gmail.com>
2023-08-13 16:08:28 +0600

    added resizeDemo file

---
+++ Auto2/src/test/java/ui/resizeDemo.java
@@ -0,0 +1,42 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class resizeDemo {
+
+	public static String browser = "Chrome";
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		if (browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equals("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equals("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
+		driver.get("https://jqueryui.com/resizable/");
+		driver.manage().window().maximize();
+		WebElement frame = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[1]/iframe"));
+		driver.switchTo().frame(frame);
+
+		WebElement element = driver.findElement(By.xpath("/html/body/div/div[3]"));
+
+		Actions action = new Actions(driver);
+		action.dragAndDropBy(element, 200, 50).perform();
+
+	}
+
+}
