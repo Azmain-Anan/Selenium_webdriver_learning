7c689e9e1f18dfae57a1df362eccd05e8b2723ba
Azmain Anan <azmainanan1998@gmail.com>
2023-08-15 20:12:56 +0600

    added implicit

---
+++ Auto2/src/test/java/ui/implicitWaitDemo.java
@@ -0,0 +1,43 @@
+package ui;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class implicitWaitDemo {
+
+	public static String browser = "Chrome";
+	public static WebDriver driver;
+
+	public static void main(String[] args){
+		if (browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equals("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equals("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
+		driver.get("https://www.ebay.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement element = driver.findElement(By.xpath("/html/body/div[5]/div[1]/ul/li[3]/a"));
+		Actions action = new Actions(driver);
+		action.moveToElement(element).perform();
+		
+		driver.findElement(By.xpath("/html/body/div[5]/div[1]/ul/li[3]/div[2]/div[1]/nav[2]/ul/li[1]/a")).click();
+
+	}
+
+}
