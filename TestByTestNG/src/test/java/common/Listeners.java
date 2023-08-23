66043bbf995f9fa332b9f8a313d7f0aa230a776c
Azmain Anan <azmainanan1998@gmail.com>
2023-08-23 21:26:07 +0600

    Add

---
+++ TestByTestNG/src/test/java/common/Listeners.java
@@ -0,0 +1,45 @@
+package common;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+	
+	public void onTestStart(ITestResult result)
+	{
+		System.out.println("Test is starting");
+	}
+	
+	public void onTestSuccess(ITestResult result)
+	{
+		
+	}
+	
+	public void onTestFailure(ITestResult result)
+	{
+		System.out.println("test failed - screenshot taken");
+	}
+	
+	public void onTestSkipped(ITestResult result)
+	{
+		
+	}
+	
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result)
+	{
+		
+	}
+	
+	public void onStart(ITestContext context)
+	{
+		
+	}
+	
+	public void onFinish(ITestContext context)
+	{
+		
+	}
+
+
+}
